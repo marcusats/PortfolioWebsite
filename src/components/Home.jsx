acd5dc61da4e027767e3f61424d54dc275d1b1b6
Marcos Aurelio Salazar Torres <mast@Marcoss-MBP.lan>
2022-02-23 10:10:45 -0500

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Social from "./Social";
+
+const Home = () => {
+  return (
+    <>
+      <div className="tokyo_tm_home">
+        <div className="home_content">
+          <div className="avatar">
+            <div
+              className="image avatar_img"
+              style={{
+                backgroundImage: "url(assets/img/slider/Marcos.jpeg)",
+              }}
+            ></div>
+            {/* END AVATAR IMAGE */}
+          </div>
+          {/* END AVATAR */}
+          <div className="details">
+            <h3 className="name">Marcos Salazar</h3>
+            <p className="job">
+              Passionated computer engineering student based in the New York City area.
+            </p>
+            {/* END JOB */}
+            <Social />
+          </div>
+          {/* END DETAILS */}
+        </div>
+        {/* END HOME CONTENT */}
+      </div>
+      {/* END HOME */}
+    </>
+  );
+};
+
+export default Home;
